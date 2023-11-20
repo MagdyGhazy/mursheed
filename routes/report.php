04cd85c8d1137f8122efd328fb54364b0e7884bc
samiraAlaalzeer <149927853+samiraAlaalzeer@users.noreply.github.com>
2023-11-20 11:32:03 +0200

    profite cost

--- routes/report.php
+++ routes/report.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\ReportController;
+use App\Http\Controllers\MobileApi\OrderController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::group(['prefix' => 'reports'], function () {
         Route::post("fillter", [ReportController::class, "index"]);
+        Route::get('profite',[OrderController::class,'profiteCost']);
     });
 });
