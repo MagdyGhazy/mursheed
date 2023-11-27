f5fb07044c234fed89ad31f79bc2ce0f2b31b43c
hassan-aamer <hassanaamer048@gmail.com>
2023-11-27 11:43:18 +0200

    Update Route Chat

--- routes/Chat.php
+++ routes/Chat.php
@@ -3,18 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::group(['prefix' => 'chat'], function () {
 
-    Route::controller(\App\Http\Controllers\Api\Chat\MessageController::class)->group(function () {
-        Route::post('Conversation/messages', 'createConversation');
-        Route::post('messages', 'createMessage');
-        Route::get('AllMessages', 'index');
-    });
-    Route::controller(\App\Http\Controllers\Api\Chat\ReplayController::class)->group(function () {
-        Route::post('Replies', 'createReplay');
-        Route::get('AllReplies', 'index');
-    });
-    Route::controller(\App\Http\Controllers\Api\Chat\ConversationController::class)->group(function () {
-        Route::get('AllConversation', 'index');
-    });
+        Route::controller(\App\Http\Controllers\Api\Chat\MessageController::class)->group(function () {
+            Route::post('Conversation/messages', 'createConversation');
+            Route::post('messages', 'createMessage');
+            Route::get('AllMessages', 'index');
+        });
+        Route::controller(\App\Http\Controllers\Api\Chat\ReplayController::class)->group(function () {
+            Route::post('Replies', 'createReplay');
+            Route::get('AllReplies', 'index');
+        });
+        Route::controller(\App\Http\Controllers\Api\Chat\ConversationController::class)->group(function () {
+            Route::get('AllConversation', 'index');
+        });
 
+    });
 });
