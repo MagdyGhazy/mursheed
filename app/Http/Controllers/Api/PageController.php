f8761864de291ea043371c3f1da471e3ca9c1e35
Magdy <150653561+MagdyVisual@users.noreply.github.com>
2023-11-30 11:38:00 +0200

    update pages

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -49,9 +49,13 @@ class PageController extends Controller
 
         $data = $request->validated();
 
-        // Encode HTML entities
-        $data['description'] = htmlspecialchars($request['description'], ENT_QUOTES, 'UTF-8');
 
+        $htmlTagsSequence = '<a><abbr><address><area><article><aside><audio><b><base><bdi><bdo><blockquote><body><br><button><canvas><caption><cite><code><col><colgroup><data><datalist><dd><del><details><dfn><dialog><div><dl><dt><em><embed><fieldset><figcaption><figure><footer><form><h1><h2><h3><h4><h5><h6><head><header><hgroup><hr><html><i><iframe><img><input><ins><kbd><label><legend><li><link><main><map><mark><meta><meter><nav><noscript><object><ol><optgroup><option><output><p><param><picture><pre><progress><q><rp><rt><ruby><s><samp><script><section><select><small><source><span><strong><style><sub><summary><sup><svg><table><tbody><td><template><textarea><tfoot><th><thead><time><title><tr><track><u><ul><var><video><wbr>';
+
+        $data['description'] = htmlspecialchars_decode(strip_tags($request['description'], $htmlTagsSequence), ENT_HTML5);
+
+        return $data;
+        
         return $this->ControllerHandler->store("page",  $data);
     }
 
